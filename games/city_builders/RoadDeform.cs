4dce572c356d3c175e9d0764221767aadf57f1a6
indieindie7 <lonisquasar@gmail.com>
2018-05-13 21:43:27 -0300

    Update RoadDeform.cs

--- games/city_builders/RoadDeform.cs
+++ games/city_builders/RoadDeform.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+[System.Serializable]
+public struct ExtrudeShape
+{
+    public Vector2[] vertices;
+
+    public float[] uCoord;
+    public int[] triangles;
+
+}
+
 
 public class RoadDeform : MonoBehaviour
 {
 
     public GameObject g1, g2, g3, g4;
-  //  public Vector3 p1, p2, p3, p4;
-  Vector3[] original;
+    Vector3[] original;
+    Mesh mesh2, mesh;
+    public ExtrudeShape shape;
 
-Mesh mesh2, mesh;
-
-    public GameObject tg;
+    public float division;
     void Start()
     {
 
 
-        mesh = tg.GetComponent<MeshFilter>().mesh;
         mesh2 = new Mesh();
 
         p = new GameObject();
@@ -35,42 +43,40 @@ Mesh mesh2, mesh;
     void Update()
     {
         int quantidades = 30;
-        Extrude(mesh, quantidades);
+        Extrude(shape, quantidades);
         GetComponent<MeshFilter>().mesh = mesh2;
-       
+
     }
-    public void Extrude(Mesh mesh, int quantidades)//, ExtrudeShape shape, OrientedPoint[] path)
+    public void Extrude(ExtrudeShape mesh, int quantidades)//, ExtrudeShape shape, OrientedPoint[] path)
     {
-        print(mesh.vertices.Length);
         int vertsInShape = mesh.vertices.Length;
-        int segments = quantidades ;
+        int segments = quantidades;
         int edgeLoops = quantidades;
         int vertCount = vertsInShape * edgeLoops;
         int triCount = (mesh.vertices.Length - 1) * segments;
         int triIndexCount = triCount * 3;
 
-        print(triCount);
         List<int> triangles = new List<int>();
 
         List<Vector3> vertices = new List<Vector3>();
-
+        List<Vector2> uvs = new List<Vector2>();
         for (int i = 0; i < quantidades + 1; i++)
         {
 
             p.transform.position = getPoint((float)i / (float)quantidades);
             p.transform.rotation = getOrientation((float)i / (float)quantidades);
-            // int offset = i * vertsInShape;
+
+
             for (int j = 0; j < vertsInShape; j++)
             {
 
 
-                f.transform.localPosition = mesh.vertices[j];
-                //  int id = offset + j;
-                vertices.Add(f.transform.position - transform.position);
-
+                f.transform.localPosition = mesh.vertices[j]/division;
+                vertices.Add(f.transform.position - transform.position); //+ p.transform.position
+           uvs.Add(new Vector2(shape.uCoord[j], Sample( quantidades , (float)i / ((float)edgeLoops) )));
             }
         }
-        //int ti = 0;
+
 
         for (int i = 0; i < quantidades; i++)
         {
@@ -82,28 +88,24 @@ Mesh mesh2, mesh;
                 int c = offset + mesh.triangles[l + 1];
                 int d = offset + mesh.triangles[l + 1] + vertsInShape;
 
-
-                triangles.Add(c);// ti++;
-                triangles.Add(b);// ti++;
-                triangles.Add(a); // ti++;
-
-                triangles.Add(c);// ti++;
-                triangles.Add(a);// ti++;
-                triangles.Add(d);// ti++;
+                triangles.Add(a);
+                triangles.Add(b);
+                triangles.Add(c);
+                triangles.Add(c);
+                triangles.Add(d);
+                triangles.Add(a);
 
             }
 
 
         }
 
-        // mesh.Clear();
         mesh2.vertices = vertices.ToArray();
         mesh2.triangles = triangles.ToArray();// triangleIndices;
         mesh2.RecalculateNormals();
-        //  mesh2.normals = normals;
-        //  mesh2.uv = uvs;
+         mesh2.uv = uvs.ToArray();
 
-
+        print(mesh.triangles.Length + " " + mesh.vertices.Length + " " + mesh2.triangles.Length + " " + mesh2.vertices.Length);
 
     }
 
@@ -150,6 +152,46 @@ Mesh mesh2, mesh;
         Vector3 tng = getTangent(t);
         Vector3 nrm = getNormal(t);
         return Quaternion.LookRotation(tng, nrm);
+    }
+
+
+      float Sample(int quantidades, float t)
+    {
+        float[] fArr = CalcLengthTableInto(quantidades);
+        int count = fArr.Length;
+        if (count == 0)
+        {
+            Debug.LogError("Unable to sample array - it has no elements");
+            return 0;
+        }
+        if (count == 1)
+            return fArr[0];
+        float iFloat = t * (count - 1);
+        int idLower = Mathf.FloorToInt(iFloat);
+        int idUpper = Mathf.FloorToInt(iFloat + 1);
+        if (idUpper >= count)
+            return fArr[count - 1];
+        if (idLower < 0)
+            return fArr[0];
+        return Mathf.Lerp(fArr[idLower], fArr[idUpper], iFloat - idLower);
+    }
+
+    float[] CalcLengthTableInto(int quantidades)
+    {
+        float[] arr = new float[quantidades];
+        arr[0] = 0f;
+        float totalLength = 0f;
+        Vector3 prev = g1.transform.position;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            float t = ((float)i) / (arr.Length - 1);
+            Vector3 pt = getPoint(t);
+            float diff = (prev - pt).magnitude;
+            totalLength += diff;
+            arr[i] = totalLength;
+            prev = pt;
+        }
+        return arr;
     }
 
 
