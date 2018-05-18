2be9a3416974fa321cacc0cd5609db57592eed7d
indieindie7 <lonisquasar@gmail.com>
2018-05-18 15:45:25 -0300

    Create Connections.cs

---
+++ games/city_builders/Roadsv2/Connections.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connections : MonoBehaviour {
+
+    public List<BasicRoad> trilhos = new List<BasicRoad>();
+	// Use this for initialization
+
+}
